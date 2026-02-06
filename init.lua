b499429f2ea7bd513fa856786ea3924480644096
Martin Townshend <martin.townshend@stspayments.com>
2026-02-06 13:29:37 +0000

    allow <Esc><Esc> keystroke to exit terminal mode

--- init.lua
+++ init.lua
@@ -73,6 +73,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- exit terminal with EscEsc
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
 if vim.g.neovide then
   vim.o.guifont = '0xProto Nerd Font Mono:h12'
 end
